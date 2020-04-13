b9675019fd771604f163328822ec98d014fdf7c3
Daruria <56038721+Daruria@users.noreply.github.com>
2020-04-13 21:41:36 +0900

    Delete

--- Framework/Copy.bat
+++
@@ -1,9 +0,0 @@
-xcopy	/y		.\Engine\Header\*.h			.\Reference\Header\
-xcopy	/y		.\Engine\Header\*.inl		.\Reference\Header\
-
-
-xcopy	/y		.\Engine\System\Code\*.h	.\Reference\Header\
-
-xcopy	/y		.\Engine\System\Bin\System.dll	.\Client\Bin\
-
-xcopy	/y		.\Engine\System\Bin\System.lib	.\Reference\Library\
