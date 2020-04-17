f2581215c20145177dc91bcc34d4328cd1c7bb3f
ems789 <32154246+ems789@users.noreply.github.com>
2020-04-17 19:29:00 +0900

    TerrainTex 교체

--- Framework/Engine/Resources/Code/TerrainTex.h
+++ Framework/Engine/Resources/Code/TerrainTex.h
@@ -7,36 +7,40 @@ BEGIN(Engine)
 
 class ENGINE_DLL CTerrainTex : public CVIBuffer
 {
+private:
 	explicit CTerrainTex(LPDIRECT3DDEVICE9 pGraphicDev);
 	explicit CTerrainTex(const CTerrainTex& rhs);
-	virtual ~CTerrainTex();
+	virtual ~CTerrainTex(void);
 
 public:
 	const _vec3*		Get_VtxPos(void) const { return m_pPos; }
 	_ulong				Get_VtxCntX(void) const { return m_dwCntX; }
 	_ulong				Get_VtxCntZ(void) const { return m_dwCntZ; }
-
 public:
-	HRESULT Ready_Buffer(const _tchar* pPath, const _ulong& dwCntX, const _ulong& dwCntZ, const _ulong& dwVtxItv = 1);
+	HRESULT Ready_Buffer(const _ulong& dwCntX, 
+						const _ulong& dwCntZ,	
+						const _ulong dwVtxItv = 1) ;
 
 private:
 	HANDLE				m_hFile;
 	_ulong				m_dwCntX;
 	_ulong				m_dwCntZ;
-	BITMAPFILEHEADER	m_fH;
-	BITMAPINFOHEADER	m_iH;
 
 	_vec3*				m_pPos;
 	_bool				m_bClone;
 
+
 public:
-	static CTerrainTex* Create(LPDIRECT3DDEVICE9 pGraphicDev, const _tchar* pPath, const _ulong& dwCntX, const _ulong& dwCntZ, const _ulong& dwVtxItv = 1);
-	virtual CResources*	Clone() override;
-	
+	static CTerrainTex*		Create(LPDIRECT3DDEVICE9 pGraphicDev, 
+							const _ulong& dwCntX, 
+							const _ulong& dwCntZ, 
+							const _ulong& dwVtxItv);
+
+	virtual CResources* Clone(void) override;
+
 private:
-	virtual void Free();
+	virtual void Free(void);
 };
 
 END
-
 #endif // TerrainTex_h__
